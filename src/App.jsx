89e3ce6d36453dba3f4338dd0e9a601e53eb3c39
M R M Qaaid <qaaidrizan113@gmail.com>
2025-07-17 20:54:58 +0530

    Last

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { Toaster } from 'react-hot-toast';
 import Home from './Pages/Home/Home';
-import Projectweb from './Pages/Projectweb/Projectweb';
+import WebProject from './Component/WebProject/WebProject';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -17,7 +17,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/projectweb" element={<Projectweb />} />
+            <Route path="/webproject" element={<WebProject />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
